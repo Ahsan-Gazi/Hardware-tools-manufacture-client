a4884f93e8b52a062bd7d96d3f6183adb6517f16
Ahsan-Gazi <ahsangazi01@gmail.com>
2022-05-22 18:22:07 +0600

    navbar desigh

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Blogs from './Pages/Blogs/Blogs';
+import Dashboard from './Pages/Dashboard/Dashboard';
 import Home from './Pages/Home/Home';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -12,6 +15,8 @@ function App() {
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
        <Route path='/about' element={<About></About>}></Route>
+       <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+       <Route path='dashboard' element={<RequireAuth><Dashboard></Dashboard></RequireAuth>}></Route>
      </Routes>
     </div>
   );
